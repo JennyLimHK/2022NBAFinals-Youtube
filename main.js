0572f27d3ff084bb3cb6e73ac56f70913d5aacca
Jenny Lim <jennylim.it@gmail.com>
2022-08-07 15:49:10 +0800

    Initial commit of AR demo with streaming Youtube video

---
+++ main.js
@@ -0,0 +1,55 @@
+import { CSS3DObject } from '../../libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js';
+const THREE = window.MINDAR.IMAGE.THREE;
+
+const createYoutube = () => {
+  return new Promise((resolve, reject) => {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    const onYouTubeIframeAPIReady = () => {
+      const player = new YT.Player('player', {
+  videoId: '04lMj66I5x8',
+  width: 1280,
+  height: 720,
+	events: {
+	  onReady: () => {
+	    resolve(player);
+	  }
+	}
+      });
+    }
+    window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    const player = await createYoutube();
+
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: './assets/targets/2022NBAFinals-03Game.mind',
+    });
+    const {renderer, cssRenderer, scene, cssScene, camera} = mindarThree;
+
+    const obj = new CSS3DObject(document.querySelector("#ar-div"));
+    const cssAnchor = mindarThree.addCSSAnchor(0);
+    cssAnchor.group.add(obj);
+
+    cssAnchor.onTargetFound = () => {
+      player.setVolume(100);
+      player.playVideo();
+    }
+    cssAnchor.onTargetLost = () => {
+      player.pauseVideo();
+    }
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      cssRenderer.render(cssScene, camera);
+    });
+  }
+  start();
+});
